b6a17bc21c566dc95f4b0b29326396e7143523bd
Shreya Sapale <Shreyasapale@users.noreply.github.com>
2020-11-20 16:47:52 +0530

    Error messages added

--- CovidTracker/accounts/views.py
+++ CovidTracker/accounts/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib import messages
 from django.contrib.auth.models import User, auth
 
 # Create your views here.
