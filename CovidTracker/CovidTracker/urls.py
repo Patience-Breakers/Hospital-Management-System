49b2c6b4c1b1b5c32adecc4f03790ddd40d1a00e
AKSHATA-22 <akshatasangwai1234@gmail.com>
2020-11-20 14:31:36 +0530

    some changes

--- CovidTracker/CovidTracker/urls.py
+++ CovidTracker/CovidTracker/urls.py
@@ -21,5 +21,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('account/', include('accounts.url')),
+    path('accounts/', include('accounts.urls')),
 ]
