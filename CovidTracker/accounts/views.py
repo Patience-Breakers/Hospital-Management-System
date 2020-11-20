c187994342be025dc1390372bc42a0686ab6d0e6
AKSHATA-22 <akshatasangwai1234@gmail.com>
2020-11-20 17:38:34 +0530

    pat done

--- CovidTracker/accounts/views.py
+++ CovidTracker/accounts/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render,redirect
 from django.contrib import messages
 from django.contrib.auth.models import User, auth
-
+from .models import patient
 
 # Create your views here.
 def index(request):
+    if request.method == 'POST':
+        pat =patient()
+        pat.name = request.POST['name']
+        pat.age = request.POST['age']
+        pat.gender = request.POST['gender']
+        pat.temperature = request.POST['temperature']
+        pat.o2level = request.POST['o2level']
+        pat.date = request.POST['date']
+        pat.address = request.POST['address']
+        pat.symptoms = request.POST['sympomts']
+        pat.existingdisease = request.POST['existingdisease']
+        pat.status = request.POST['status']
+        pat.save()
     return render(request, 'index.html')
 def login(request):
     if request.method == 'POST':
