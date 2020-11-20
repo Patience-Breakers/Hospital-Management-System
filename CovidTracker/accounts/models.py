9220c480e0fe443f3d6715cc5fc3db5ccc9ecdfd
AKSHATA-22 <akshatasangwai1234@gmail.com>
2020-11-20 14:18:43 +0530

    model created

--- CovidTracker/accounts/models.py
+++ CovidTracker/accounts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class patient(models.Model):
+    name = models.TextField()
+    temperature = models.TextField()
+    o2level= models.TextField()
+    date = models.TextField()
+    address = models.TextField()
+    symptoms = models.TextField()
+    exitingdisease =models.BooleanField()
+  
