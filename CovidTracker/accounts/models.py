f85849c02b2ab20ea5e18b19ece294c6e7c4f685
Shreya Sapale <Shreyasapale@users.noreply.github.com>
2020-11-21 12:35:43 +0530

    New models created

--- CovidTracker/accounts/models.py
+++ CovidTracker/accounts/models.py
@@ -2,16 +2,45 @@ from django.db import models
 
 # Create your models here.
 
+from django.db import models
+
+# Create your models here.
+
 class patient(models.Model):
     name = models.TextField()
+
+
+    name = models.TextField()
+    photo = models.ImageField( upload_to='pics')
     age = models.IntegerField()
     gender = models.CharField(max_length=10) # M of F 
-    temperature = models.TextField()
-    o2level= models.TextField()
-    date = models.TextField()
-    address = models.TextField()
-    symptoms = models.TextField()
-    exitingdisease =models.TextField()
+    phone = models.IntegerField
+    address = models.TextField
+    emailId = models.EmailField(max_length=254)
+    date = models.DateField( auto_now=False, auto_now_add=False)
+
+    #Hospital
+
+    bedNo = models.IntegerField()
+    floor = models.IntegerField()
+    o2level = models.IntegerField()
+    asymptomatic = models.BooleanField(default=False)
+    cough = models.BooleanField(default=False)
+    fever = models.BooleanField(default=False)
+    apetiteLoss = models.BooleanField()
+    temperature = models.IntegerField()
     status = models.CharField(max_length=20)
     # admitted , recovered , decreased
-  
+
+    onVentilator = models.BooleanField(default=False)
+
+
+    thirdastCOvidResult = models.BooleanField(default=True)
+    secondlastCovidResult = models.BooleanField(default=True)
+    lastCovidResult = models.BooleanField(default=True)#true for covid positive
+
+    #If(all three are false)
+
+    daysAdmitted = models.IntegerField()
+    doctorId = models.IntegerField()
+    
